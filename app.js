e02b376b702b21f18cf083c2a1bdcb5626439ce2
julianll1202 <a20490712@itmexicali.edu.mx>
2022-08-27 11:32:30 -0700

    Added deleteTask() function

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const taskInput=document.getElementById("task");
 const taskList=document.getElementById("task-list");
-
+let c=0;
 //add function 
 function addTask(){
+    //store the text input on task
     let task=taskInput.value;
+    //call display function
     displayTask(task);
+    //call clear function
     clearInput();
+    //increase task counter
+    c++;
 }
 
 //display function
-function displayTask(taskName){
-    taskList.innerHTML += `<li>${taskName}</li>`;
+function displayTask(newTask){
+    //add task text to the list alongside a delete button
+    taskList.innerHTML += `<li id="${c}">${newTask} <button onclick="deleteTask(${c})">Delete</button> </li>`;
 }
 
 //clear function
 function clearInput(){
+    //clears the input bar
     taskInput.value="";
     taskInput.focus();
 }
 
 //delete function
-function deleteTask(){
+function deleteTask(taskID){
+    //Deletes the task that has the specified ID
+    document.getElementById(taskID).remove();
 }
